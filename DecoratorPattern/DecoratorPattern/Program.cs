b0ab3250dd3ba83ae0eeda8cadedda083619b1a1
divinegraceMll <144877426+divinegraceMll@users.noreply.github.com>
2023-11-17 07:09:52 +0800

    Add files via upload

---
+++ DecoratorPattern/DecoratorPattern/Program.cs
@@ -0,0 +1,84 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        // Client code using the Decorator Pattern
+        IComponent component = new ConcreteComponent();
+        Console.WriteLine("Client: Basic component:");
+        component.Operation();
+
+        // Decorate the component with ConcreteDecoratorA
+        IComponent decoratedComponentA = new ConcreteDecoratorA(component);
+        Console.WriteLine("\nClient: Decorated component with ConcreteDecoratorA:");
+        decoratedComponentA.Operation();
+
+        // Decorate the component with ConcreteDecoratorB
+        IComponent decoratedComponentB = new ConcreteDecoratorB(decoratedComponentA);
+        Console.WriteLine("\nClient: Decorated component with ConcreteDecoratorB:");
+        decoratedComponentB.Operation();
+    }
+
+
+    // Component interface
+    interface IComponent
+    {
+        void Operation();
+    }
+
+    // Concrete component
+    class ConcreteComponent : IComponent
+    {
+        public void Operation()
+        {
+            Console.WriteLine("ConcreteComponent: Performing operation.");
+        }
+    }
+
+    // Base decorator class
+    abstract class Decorator : IComponent
+    {
+        protected IComponent component;
+
+        public Decorator(IComponent component)
+        {
+            this.component = component;
+        }
+
+        public virtual void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+    // Concrete decorator 1
+    class ConcreteDecoratorA : Decorator
+    {
+        public ConcreteDecoratorA(IComponent component) : base(component)
+        {
+        }
+
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("ConcreteDecoratorA: Added behavior.");
+        }
+    }
+
+    // Concrete decorator 2
+    class ConcreteDecoratorB : Decorator
+    {
+        public ConcreteDecoratorB(IComponent component) : base(component)
+        {
+        }
+
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("ConcreteDecoratorB: Added different behavior.");
+        }
+    }
+}
---
+++ FacadePattern/FacadePattern/Program.cs
@@ -0,0 +1,69 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Using the Facade to simplify interactions with the subsystem
+        Facade facade = new Facade();
+
+        // Client code interacts with the subsystem through the facade
+        facade.OperationA();
+        facade.OperationB();
+    }
+
+    // Subsystem 1
+    class Subsystem1
+    {
+        public void Operation1()
+        {
+            Console.WriteLine("Subsystem1: Operation1");
+        }
+
+        public void Operation2()
+        {
+            Console.WriteLine("Subsystem1: Operation2");
+        }
+    }
+
+    // Subsystem 2
+    class Subsystem2
+    {
+        public void Operation3()
+        {
+            Console.WriteLine("Subsystem2: Operation3");
+        }
+
+        public void Operation4()
+        {
+            Console.WriteLine("Subsystem2: Operation4");
+        }
+    }
+
+    // Facade
+    class Facade
+    {
+        private Subsystem1 subsystem1;
+        private Subsystem2 subsystem2;
+
+        public Facade()
+        {
+            subsystem1 = new Subsystem1();
+            subsystem2 = new Subsystem2();
+        }
+
+        // Facade methods that simplify the interactions with the subsystem
+        public void OperationA()
+        {
+            Console.WriteLine("Facade: OperationA");
+            subsystem1.Operation1();
+            subsystem2.Operation3();
+        }
+
+        public void OperationB()
+        {
+            Console.WriteLine("Facade: OperationB");
+            subsystem1.Operation2();
+            subsystem2.Operation4();
+        }
+    }
+
+}
